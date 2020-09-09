43d79ab1cbf20d6bbf5adc2c5d66147931f363a7
David Maas <contact@pathogenstudios.com>
2020-09-09 15:37:20 -0500

    Added OutputSession for managing all the files written for a translation.
    This type primarily solves accidentally overwriting files written previously during translation, removing the hacked-in support in CodeWriter. This will also aid in cleaning previous output since it keeps track of files written.

---
+++ Biohazrd.OutputGeneration/ProvidesOutputSessionFactoryAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Biohazrd.OutputGeneration
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class ProvidesOutputSessionFactoryAttribute : Attribute
+    { }
+}
