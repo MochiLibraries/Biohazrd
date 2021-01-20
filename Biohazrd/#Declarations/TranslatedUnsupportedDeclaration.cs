4068fc28c4ef3de0a4f2481c0f0e445db00f9aae
David Maas <contact@pathogenstudios.com>
2021-01-19 18:44:10 -0600

    Added constructors for synthesizing TranslatedUnsupportedDeclaration.
    Synthesizing these types can be useful for emitting errors from certain types of transformations. This makes the name somewhat misleading, but it's easier than making transformations keep track of diagnostics and adding them at the end.

--- Biohazrd/#Declarations/TranslatedUnsupportedDeclaration.cs
+++ Biohazrd/#Declarations/TranslatedUnsupportedDeclaration.cs
@@ -8,6 +8,14 @@ namespace Biohazrd
             : base(file, declaration)
             => Diagnostics = Diagnostics.Add(severity, declaration, reason);
 
+        public TranslatedUnsupportedDeclaration(Decl declaration, Severity severity, string reason)
+            : this(TranslatedFile.Synthesized, declaration, severity, reason)
+        { }
+
+        public TranslatedUnsupportedDeclaration(Severity severity, string reason)
+            : base(TranslatedFile.Synthesized)
+            => Diagnostics = Diagnostics.Add(severity, reason);
+
         public override string ToString()
             => $"Unsupported Declaration {base.ToString()}";
     }
