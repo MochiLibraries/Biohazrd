07ede07af513caeab7962be1adcdc39484bdc575
David Maas <contact@pathogenstudios.com>
2021-07-09 09:24:26 -0500

    Added attributes for Linux/Windows-specific tests.

---
+++ Tests/Biohazrd.Tests.Common/#TestConditions/LinuxFactAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Biohazrd.Tests.Common
+{
+    public sealed class LinuxFactAttribute : FactAttribute
+    {
+        public LinuxFactAttribute()
+        {
+            if (Skip is null && !OperatingSystem.IsLinux())
+            { Skip = "This test can only be executed on Linux."; }
+        }
+    }
+}
---
+++ Tests/Biohazrd.Tests.Common/#TestConditions/LinuxTheoryAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Biohazrd.Tests.Common
+{
+    public sealed class LinuxTheoryAttribute : TheoryAttribute
+    {
+        public LinuxTheoryAttribute()
+        {
+            if (Skip is null && !OperatingSystem.IsLinux())
+            { Skip = "This test can only be executed on Linux."; }
+        }
+    }
+}
